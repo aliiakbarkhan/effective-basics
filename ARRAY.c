a07724a431e672ea074abd363d5defaa1ab04c87
Ali Akbar Khan <56528027+aliiakbarkhan@users.noreply.github.com>
2024-10-25 11:21:53 +0530

    updates

--- ARRAY.c
+++ ARRAY.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
-int main(int argc, char const *argv[])
-{
-   int a[100],i;
-   for ( i = 0; i <= 9; i++)
-   {
-   printf("enter value of a[%d]:",i);
-   scanf("%d",&a[i]);
-
-   }
-   for ( i = 0; i < 10; i++)
-   {
-   printf("%d",a[i]);
-    /* code */
-   }
-   
-   
-    return 0;
+int main(){
+	int i;
+	int mark[10];
+	for(i = 0; i<10; i++){
+		printf("Enter %d student mark:",i+1);
+		scanf("%d",&mark[i]);
+	}
+	for(i = 0; i<10; i++){
+		if(mark[i]<35){
+			printf("%d\n",i);
+		}
+	}
+	return 0;
 }
